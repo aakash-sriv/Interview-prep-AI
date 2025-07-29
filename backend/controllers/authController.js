cce500c0a439131f41e1416d2dd8a6427a879a2b
aakash-sriv <aakashsriv06@gmail.com>
2025-07-29 23:34:56 +0530

    temp

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,8 +12,7 @@ const generateToken = (userId) => {
 // @access Public
 const registerUser = async (req , res) => {
     try{
-        const {name ,email , password , profileImageUrl} =
-        req.body;
+        const {name ,email , password , profileImageUrl} =req.body;
 
         //check if user already exists
         const userExists = await User.findOne({email});
@@ -45,12 +44,46 @@ const registerUser = async (req , res) => {
 // @access Public
 
 const loginUser = async (req , res) => {
+    try {
+        const {email ,password} =req.body;
+
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(500).json({message: "Invalid email or password"});
+        }
+
+        //compare password
+        const isMatch = await bcrypt.compare(password , user.password);
+        if(!isMatch){
+            return res.status(500).json({message: "Invalid email or password"});
+        }
+
+        //return user data with JWT
+        res.json({
+            _id : user._id,
+            name : user.name ,
+            email: user.email ,
+            profileImageUrl : user.profileImageUrl,
+            token : generateToken(user._id),            
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
 };
 // @desc get user profile
 // @route GET /api/auth/login
 // @access private(requires JWT)
 
 const getUserProfile = async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if(!user){
+            return res.status(404).json({message : "User not found"})
+        }
+        res.json(user);f
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }    
 };
 
 module.exports = { registerUser ,loginUser , getUserProfile };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { registerUser , loginUser , getUserProfile } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
-
+const upload = require("../middlewares/uploadMiddleware");
 
 const router = express.Router();
 
@@ -9,5 +9,16 @@ const router = express.Router();
 router.post("/register" , registerUser); //register user
 router.post("/login" , loginUser); // login user
 router.get("/profile" , protect , getUserProfile); //get user profile
+
+router.post("/upload-image", upload.single("image"), (req , res) => {
+    if(!req.file){
+        return res.status(400).json({ message: "No file uploaded"});
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+    }`;
+    res.status(200).json({ imageUrl });
+})
+
 
 module.exports = router;

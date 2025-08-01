c4a1b564ce5cb135df29b1df997679c816752f93
aakash-sriv <aakashsriv06@gmail.com>
2025-08-02 01:59:59 +0530

    temp

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,5 +1,6 @@
 const {GoogleGenAI} = require('@google/genai');
 const { conceptExplainPrompt, questionAnswerPrompt } = require("../utils/prompts");
+const { raw } = require('express');
 
 const ai = new GoogleGenAI({ apiKey : process.env.GEMINI_API_KEY });
 
@@ -26,8 +27,14 @@ const generateInterviewQuestions = async (req , res) => {
 
         //Clean it : Remove ```json and ``` from beginning and end
         const cleanedText = rawText
-            .replace()
-            .replace
+            .replace(/^```json\s*/,"") // remove starting ```json
+            .replace(/```$/, "") //remove ending ```
+            .trim(); // remove extra spaces
+
+        // Now safe to parse
+        const data = JSON.parse(cleanedText);
+
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json ({
             message : "Failed to generate questions",
@@ -41,7 +48,38 @@ const generateInterviewQuestions = async (req , res) => {
 // @access Private
 
 const generateConceptExplanation = async (req , res) => {
+    try {
+        const {question} = req.body;
 
+        if(!question) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const prompt = conceptExplainPrompt(question);
+
+        const response = await ai.models.generateContent({
+            model : "gemini-2.0-flash-lite" , 
+            contents : prompt,
+        });
+
+        let rawText = response.text;
+
+        //clean it: remove ```json and ``` from beginning and end
+        const cleanedText = rawText
+            .replace(/^```json\s*/,"") // remove starting ```json
+            .replace(/```$/, "") // remove ending ``` 
+            .trim(); // remove extra spaces
+
+            //Now safe for parse
+            const data = JSON.parse(cleanedText);
+
+            res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json ({
+            message : "Failed to generate questions",
+            error : error.message,
+        });
+    }
 };
 
 module.exports = { generateInterviewQuestions, generateConceptExplanation};

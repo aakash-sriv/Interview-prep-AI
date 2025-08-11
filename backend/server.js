4406e4b5888cb681a75e10c6f632bcb24333a8ca
aakash-sriv <aakashsriv06@gmail.com>
2025-08-11 17:30:08 +0530

    temp

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,9 @@ app.use("/uploads", express.static(path.join(__dirname , "uploads"), {}));
 //Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// for testing
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
